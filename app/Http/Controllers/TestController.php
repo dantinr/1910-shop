4260ce68d14a8e3cdabb4cdc9fe7677adc30f6a6
dante <dantinr@gmail.com>
2020-07-16 10:11:51 +0800

    test1

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,8 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
+use DB;
 
 class TestController extends Controller
 {
 
+    public function test1()
+    {
+        $data = DB::table('p_goods')->first();
+        var_dump($data);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test1','TestController@test1');
+
 
 
