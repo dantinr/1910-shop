762769209019324c69db2795192fd3c7ba44f880
dante <dantinr@gmail.com>
2020-08-13 14:08:30 +0800

    排行榜 伪代码

--- app/Http/Controllers/Goods/RankController.php
+++ app/Http/Controllers/Goods/RankController.php
@@ -8,8 +8,60 @@ use Illuminate\Http\Request;
 class RankController extends Controller
 {
 
-    public function index()
+    /**
+     * 排行榜 默认为 浏览排行
+     * 浏览排行 ss:goods_view:count
+     * 购买排行 ss:goods_buy:count
+     * 收藏排行 ss:goods_fav:count
+     */
+    public function index(Request $request)
     {
-        return view('goods.rank');
+        $type = $request->get('type',1);      // 1浏览排行 2购买排行 3收藏排行
+
+        switch ($type)
+        {
+            case 1 :
+                $list = $this->viewList();
+                break;
+            case 2 :
+                $list = $this->buyList();
+                break;
+            case 3 :
+                $list = $this->favList();
+                break;
+            default :
+                $list =  $this->viewList();
+                break;
+        }
+
+        $data = [
+            'list'  => $list
+        ];
+
+        return view('goods.rank',$data);
+    }
+
+    /**
+     * 浏览排行
+     */
+    protected function viewList()
+    {
+
+    }
+
+    /**
+     * 购买排行
+     */
+    protected function buyList()
+    {
+
+    }
+
+    /**
+     * 收藏排行
+     */
+    protected function favList()
+    {
+
     }
 }
