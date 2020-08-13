33dd759d3a2fadf58e892e1d07ff3965f5a9c800
dante <dantinr@gmail.com>
2020-08-13 18:24:05 +0800

    github登录

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,26 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 use DB;
+use GuzzleHttp\Client;
 
 class TestController extends Controller
 {
 
     public function test1()
     {
-        $goods_info = [
-            'goods_id'  => 12345,
-            'goods_name'    => 'IPhoneX',
-            'price'     =>  800000,
-            'add_time'  => 123123123
-        ];
 
-        $key = 'goods_12345';
 
-        Redis::hmset($key,$goods_info);
+        $url = 'https://api.github.com/user';
+        $token = '7e59cb7e5c0debab9b77472e6c33b73bc16c62ee';
+        $token = 'cccccc';
+        $client = new Client();
+        $response = $client->request('GET',$url,[
+            'headers'   => [
+                'Authorization' => $token . ' OAUTH-TOKEN'
+            ]
+        ]);
+        echo $response->getBody();
+
     }
 
     public function test2()
     {
-        
+
     }
 }
--- app/Http/Controllers/User/OauthController.php
+++ app/Http/Controllers/User/OauthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use function GuzzleHttp\Psr7\parse_query;
 
 class OauthController extends Controller
 {
@@ -18,8 +19,9 @@ class OauthController extends Controller
         $code = $_GET['code'];
 
         //换取access_token
-        $this->getToken($code);
-        echo '<pre>';print_r($_GET);echo '</pre>';die;
+        $token = $this->getToken($code);
+        $user_info = $this->getGithubUserInfo($token);
+        echo '<pre>';print_r($user_info);echo '</pre>';
     }
 
 
@@ -31,16 +33,33 @@ class OauthController extends Controller
         $url = 'https://github.com/login/oauth/access_token';
 
         //post 接口  Guzzle or  curl
-        $token = "";
-        // 拿到token  获取用户信息
-
-        $this->getGithubUserInfo($token);
+        $client = new Client();
+        $response = $client->request('POST',$url,[
+            'form_params'   => [
+                'client_id'         => env('OAUTH_GITHUB_ID'),
+                'client_secret'     => env('OAUTH_GITHUB_SEC'),
+                'code'              => $code
+            ]
+        ]);
+        parse_str($response->getBody(),$str); // 返回字符串 access_token=59a8a45407f1c01126f98b5db256f078e54f6d18&scope=&token_type=bearer
+        return $str['access_token'];
     }
 
 
+    /**
+     * 获取github个人信息
+     * @param $token
+     */
     protected function getGithubUserInfo($token)
     {
         $url = 'https://api.github.com/user';
         //GET 请求接口
+        $client = new Client();
+        $response = $client->request('GET',$url,[
+            'headers'   => [
+                'Authorization' => $token . ' OAUTH-TOKEN'
+            ]
+        ]);
+        echo $response->getBody();
     }
 }
